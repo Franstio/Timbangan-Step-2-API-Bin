6498216f30a6320f21b91c44e652550fe42bb22e
FahriEfendi <fahriefendi0795@gmail.com>
2024-05-20 22:17:18 +0700

    .

--- controllers/TriggerLock.js
+++ controllers/TriggerLock.js
@@ -1,4 +1,5 @@
 import ModbusRTU from 'modbus-serial';
+import os from 'os';
 const client = new ModbusRTU();
 client.connectRTU("/dev/ttyUSB0", { baudRate: 9600 });
 client.setTimeout(5000);
@@ -31,3 +32,35 @@ export const lockTop = async (req, res) => {
         res.status(500).json({ msg: error });
     }
 };
+
+export const lockBottom = async (req, res) => {
+    try {
+        const {idLockBottom} = req.body;
+	//console.log({id: idRollingDoor});
+
+       client.setID(idLockBottom);
+        if (!client.isOpen) {
+            client.open( () => {
+                console.log("modbus open");
+           });
+        }
+        const address = 20;
+        const value = 1;
+        const log = await client.writeRegister(address,value);
+//        const data = await client.readHoldingRegisters(address, 8);
+//        console.log({ log: log, data: data });
+       /*  if (value === 1) {
+            res.status(200).json({ msg: `Top Lock diBuka` });
+        } else {
+            res.status(200).json({ msg: `Kunci dengan address ${address} berhasil ditutup.` });
+        } */
+        res.status(200).json({ msg: `Bottom Lock diBuka` });
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+};
+
+export const getHostname = async (req,res) =>{
+    res.status(200).json({hostname: os.hostname()});
+}
--- routes/LockDoorRoute.js
+++ routes/LockDoorRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {lockTop} from "../controllers/TriggerLock.js"
+import {lockTop,lockBottom, getHostname} from "../controllers/TriggerLock.js"
 
 const router = express.Router();
 
 router.post('/locktop', lockTop);
-
+router.post('/lockbottom', lockBottom);
+router.get('/hostname',getHostname);
 
 export default router;
