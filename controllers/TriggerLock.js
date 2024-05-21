96f6ad81e5ed52d3c378320774f0f057c942b6d5
FahriEfendi <fahriefendi0795@gmail.com>
2024-05-21 10:35:49 +0700

    .

--- controllers/TriggerLock.js
+++ controllers/TriggerLock.js
@@ -1,7 +1,7 @@
-/* import ModbusRTU from 'modbus-serial';
+import ModbusRTU from 'modbus-serial';
 const client = new ModbusRTU();
 client.connectRTU("/dev/ttyUSB0", { baudRate: 9600 });
-client.setTimeout(5000); */
+client.setTimeout(5000); 
 import os from 'os';
 
 
