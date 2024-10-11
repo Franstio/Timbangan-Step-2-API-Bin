9f31b557197d5b3b44ac6962947b85d9d06e99c2
Franstio <franstiodiego01@gmail.com>
2024-10-11 13:04:22 +0700

    ok

--- controllers/ActionSensor.js
+++ controllers/ActionSensor.js
@@ -162,7 +162,7 @@ export const pushPayloadData =(data)=>{
 const writeCmd = async (data) => {
     try
     {
-        client.setTimeout(100);
+        client.setTimeout(1000);
         client.setID(data.id);
         await client.writeRegister(data.address,data.value);
         return;
