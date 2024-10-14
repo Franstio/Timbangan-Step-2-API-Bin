561d836b54183fbc420f7a6fe1fbe25b7c207ed9
Franstio <franstiodiego01@gmail.com>
2024-10-14 16:08:26 +0700

    ok

--- controllers/plcClient.js
+++ controllers/plcClient.js
@@ -1,5 +1,5 @@
 import ModbusRTU from 'modbus-serial';
 const client = new ModbusRTU();
 client.connectRTU("/dev/ttyUSB0", { baudRate: 9600 });
-client.setTimeout(100); 
+client.setTimeout(1000); 
 export default client;
