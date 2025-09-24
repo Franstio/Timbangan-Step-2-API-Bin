fecd8702a177e7b2c9648e2bb8b4e7907b7ae47e
Franstio <franstiodiego01@gmail.com>
2025-09-24 09:14:42 +0700

    Add: remove all old queue data

--- controllers/Bin.js
+++ controllers/Bin.js
@@ -249,8 +249,8 @@ export const clearTransactionBin = async ()=>{
   const redisClient = createClient();  
 
   redisClient.on('error', err => console.log('Redis Client Error', err));
-  await QueuePLC.clean(10 * 1000);
-  await SensorObserveQueue.clean(10 * 1000);
+  await QueuePLC.obliterate({force:true});
+  await SensorObserveQueue.obliterate({force:true});
   await SensorObserveQueue.add({type:'observe'},{
     removeOnFail:{count:10},timeout:3000,removeOnComplete:{count:5}
   });
